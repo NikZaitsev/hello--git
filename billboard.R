437fb69cf93c2a8ffa03d3321d0fa872ff9db4f9
Nikita Zaitsev <nikita.zajtcev@gmail.com>
2024-12-14 16:57:52 +0300

    Billboard R

--- billboard.R
+++ billboard.R
@@ -1,2 +1,10 @@
 library(readr)
 library(dplyr)
+
+top100 <- read.csv("billboard100.csv")
+top10 <- top100 %>%
+  group_by(Date) %>%
+  filter(row_number() <= 10) %>%
+  ungroup()
+
+write_csv(top100, 'billboard.csv')
